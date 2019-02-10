1dbb945ea3f154bcade3e7a01e47624203fb877b
RobotCherries <zdip14@gmail.com>
2019-02-10 12:52:13 +0200

    Update: quote section

--- src/pages/home/components/quote.js
+++ src/pages/home/components/quote.js
@@ -5,13 +5,12 @@ class Quote extends Component {
         return (
             <div className='quote__wrapper container'>
                 <blockquote className='quote'>
-                    <header className='quote__header'></header>
                     <p className='quote__content'>
                         <span className="quote__symbol">“</span>
                         Spend each day trying to be a little wiser than you were when you woke up. Day by day, and at the end, if you live long enough, you will get out of life what you deserve.
                         <span className="quote__symbol">”</span>
                     </p>
-                    <footer className='quote__footer'><cite title="Poor Charlie's Almanack: The Wit and Wisdom of Charles T. Munger">Charles T. Munger</cite></footer>
+                    <footer className='quote__footer'><cite title="Poor Charlie's Almanack: The Wit and Wisdom of Charles T. Munger">- Charles T. Munger</cite></footer>
                 </blockquote>
             </div>
         );
