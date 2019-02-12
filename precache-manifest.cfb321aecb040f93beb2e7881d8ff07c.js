70eab2364fddcd4bc12891b24701c704fb2dd039
RobotCherries <zdip14@gmail.com>
2019-02-12 14:10:38 +0200

    Updates

---
+++ precache-manifest.cfb321aecb040f93beb2e7881d8ff07c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/react_logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6daab40997d34f5ebd11528ab8b626df",
+    "url": "/portfolio/static/media/arrow_right.6daab409.svg"
+  },
+  {
+    "revision": "c353e3dabba482f3721f67c6a43f1744",
+    "url": "/portfolio/static/media/MesloLGM-Regular.c353e3da.ttf"
+  },
+  {
+    "revision": "2a64391bfb6a980844d5611b9ab3f2a8",
+    "url": "/portfolio/static/media/MesloLGM-Bold.2a64391b.ttf"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "715f5202b98db95a807c",
+    "url": "/portfolio/static/js/main.715f5202.chunk.js"
+  },
+  {
+    "revision": "0216c475c17152d72669",
+    "url": "/portfolio/static/js/1.0216c475.chunk.js"
+  },
+  {
+    "revision": "715f5202b98db95a807c",
+    "url": "/portfolio/static/css/main.fce28e65.chunk.css"
+  },
+  {
+    "revision": "0216c475c17152d72669",
+    "url": "/portfolio/static/css/1.42ccd92c.chunk.css"
+  },
+  {
+    "revision": "8d4a6368b846379b1c797d66b7ff9559",
+    "url": "/portfolio/index.html"
+  }
+];
