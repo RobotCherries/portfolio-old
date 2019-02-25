e63021560a640c259da843e7c038648956c85cbd
RobotCherries <zdip14@gmail.com>
2019-02-25 09:49:01 +0200

    Updates

---
+++ precache-manifest.1babae008b9f479a41b9b522e20fea45.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/react_logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6daab40997d34f5ebd11528ab8b626df",
+    "url": "/static/media/arrow_right.6daab409.svg"
+  },
+  {
+    "revision": "c353e3dabba482f3721f67c6a43f1744",
+    "url": "/static/media/MesloLGM-Regular.c353e3da.ttf"
+  },
+  {
+    "revision": "2a64391bfb6a980844d5611b9ab3f2a8",
+    "url": "/static/media/MesloLGM-Bold.2a64391b.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fb5a8990d0b1289d2d5a",
+    "url": "/static/js/main.fb5a8990.chunk.js"
+  },
+  {
+    "revision": "3fa7c786c390e7b51bea",
+    "url": "/static/js/1.3fa7c786.chunk.js"
+  },
+  {
+    "revision": "fb5a8990d0b1289d2d5a",
+    "url": "/static/css/main.faf3779c.chunk.css"
+  },
+  {
+    "revision": "3fa7c786c390e7b51bea",
+    "url": "/static/css/1.5fac19fa.chunk.css"
+  },
+  {
+    "revision": "70cb3569113ab2b164a2621caa3a82d1",
+    "url": "/index.html"
+  }
+];
