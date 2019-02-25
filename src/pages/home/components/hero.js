788ea5842cfedafbd0e9321504f57fe56eb17f17
RobotCherries <zdip14@gmail.com>
2019-02-25 14:36:58 +0200

    Responsive: hero title

--- src/pages/home/components/hero.js
+++ src/pages/home/components/hero.js
@@ -5,8 +5,11 @@ class Hero extends Component {
     render() {
         return (
             <div className='hero js-blobs'>
-                <h1 className='hero__title'>
+                <h1 className='hero__title hero__title--desktop'>
                     var Oana = ["Web Developer", "Graphic Designer"];
+                </h1>
+                <h1 className='hero__title hero__title--mobile'>
+                    Hello World!
                 </h1>
                 <Link className='hero__btn' to={'/development'}>View my work</Link>
             </div>
